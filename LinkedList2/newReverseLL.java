ce0bb3e8775ac2a7f781ffe4878ba792168b2a51
sourabh <sourabhbarnwal2001@gmail.com>
2022-02-20 00:52:14 +0530

    New reverse Linked List class

---
+++ LinkedList2/newReverseLL.java
@@ -0,0 +1,24 @@
+package LinkedList2;
+
+public class newReverseLL extends LInkedListUse {
+    
+    public static Node<Integer> reverseR(Node<Integer> head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node<Integer> finalHead = reverseR(head.next);
+        Node<Integer> temp = finalHead;
+        
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = head;
+        head.next = null;
+        return finalHead;
+    }
+    public static void main(String[] args) {
+        Node<Integer> head = takeInput();
+        head = reverseR(head);
+        print(head);
+    }
+}
