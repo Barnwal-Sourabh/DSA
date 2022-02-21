73a4af831cd63a4d30645378c1fc4e7bdadb2dcd
sourabh <sourabhbarnwal2001@gmail.com>
2022-02-22 02:50:00 +0530

    revese in optimize way

---
+++ LinkedList2/ReverseLL2.java
@@ -0,0 +1,39 @@
+package LinkedList2;
+
+public class ReverseLL2 extends LInkedListUse{
+    
+    public static DoubleNode reverseLLBetter(Node<Integer> head){
+
+        if(head == null || head.next == null){
+            DoubleNode ans = new DoubleNode();
+            ans.tail = head;
+            ans.head = head;
+            return ans;
+        }
+
+        DoubleNode smallAns = reverseLLBetter(head.next);
+        smallAns.tail.next = head;
+        head.next = null;
+
+        DoubleNode ans = new DoubleNode();
+        ans.head = smallAns.head;
+        ans.tail = head;
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        
+        Node<Integer> head = takeInput();
+        DoubleNode ans = reverseLLBetter(head);
+        print(ans.head);
+    }
+    
+}
+
+ class DoubleNode {
+    Node<Integer> head;
+    Node<Integer> tail;
+    
+}
+
