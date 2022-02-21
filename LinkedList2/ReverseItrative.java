a11faf7f4c8ccaf0f51755ad4554a6f70973daf5
sourabh <sourabhbarnwal2001@gmail.com>
2022-02-22 02:49:35 +0530

    Reverse Linked List Iteratively

---
+++ LinkedList2/ReverseItrative.java
@@ -0,0 +1,26 @@
+package LinkedList2;
+
+class ReverseItrative extends LInkedListUse{
+
+    public static Node<Integer> reverse_I(Node<Integer> head){
+
+        Node<Integer> curr = head;
+        Node<Integer> prev = null;
+        Node<Integer> temp;
+
+        while(curr != null){
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> head = takeInput();
+        head = reverse_I(head);
+        print(head);
+    }
+}
