1845a7ee189edd61547b2fb40763066e23fe889b
sourabh <sourabhbarnwal2001@gmail.com>
2022-02-26 10:55:14 +0530

    Collection of Linked List

---
+++ LinkedList2/CollectionLinkedLIst.java
@@ -0,0 +1,25 @@
+package LinkedList2;
+
+import java.util.LinkedList;
+
+/**
+ * CollectionLinkedLIst
+ */
+public class CollectionLinkedLIst {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.addFirst(30);
+        list.set(1, 5);
+        list.add(1, 2);
+        list.clone();
+
+        for(int i = 0; i<list.size(); i++){
+            System.out.print(list.get(i) +" ");
+        }
+        
+
+    }
+}
