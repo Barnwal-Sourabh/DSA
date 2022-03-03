d2952e89322a8c3cc7bbc4fb75d08b948d2f7a8c
sourabh <sourabhbarnwal2001@gmail.com>
2022-03-03 19:58:29 +0530

    Test class for our StackUseingArray class

--- Stack/StackUse.java
+++ Stack/StackUse.java
@@ -4,7 +4,7 @@ public class StackUse {
     
     public static void main(String[] args) throws StackFullExeception {
         
-        StackUssingArray stack = new StackUssingArray(3);
+        StackUseingArray stack = new StackUseingArray(3);
         for(int i = 1; i<=5; i++){
             stack.push(i);
         }
