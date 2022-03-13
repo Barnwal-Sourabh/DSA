e2f7dc0af3f767cc8ea3652e57999a25be550485
sourabh <sourabhbarnwal2001@gmail.com>
2022-03-14 02:55:41 +0530

    All Queue files are internal

---
+++ Queues/QueueUsingArray.java
@@ -0,0 +1,68 @@
+package Queues;
+
+public class QueueUsingArray {
+    private int data[];
+    private int front, rear, size;
+
+    public QueueUsingArray() {
+        data = new int[10];
+        front = -1;
+        rear = -1;
+        size = 0;    
+    }
+
+    public QueueUsingArray(int capacity){
+        data = new int[capacity];
+        front = -1;
+        rear = -1;
+        size = 0; 
+    }
+
+    int size(){
+        return size;
+    }
+
+    boolean isEmpty(){
+        return size == 0;
+    }
+
+    int front() throws QueueEmptyException{
+        if(size == 0){
+            throw new QueueEmptyException();
+        }
+        return data[front];
+    }
+
+    void enqueue(int element) throws QueueFullException{
+        if(size == data.length){
+            throw new QueueFullException();
+        }
+        if(size == 0){
+            front = 0;
+        }
+        size++;
+        // rear++;
+        // if(rear == data.length){
+        //     rear = 0;
+        // }
+        rear = (rear + 1) % data.length;
+        data[rear] = element;
+    }
+
+    int dequeue() throws QueueEmptyException{
+        if(size == 0){
+            throw new QueueEmptyException();
+        }
+        size--;
+        int temp = data[front];
+        // front++;
+        // if(front == data.length){
+        //     front = 0;
+        // }
+        front = (front +1) % data.length;
+        return temp;
+
+
+    }
+}
+
