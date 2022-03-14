909c723ee91afd75275391603f91e424ad884f3e
sourabh <sourabhbarnwal2001@gmail.com>
2022-03-14 16:53:41 +0530

    To check doubleCapacity function

--- Queues/QueueUse.java
+++ Queues/QueueUse.java
@@ -3,7 +3,8 @@ package Queues;
 public class QueueUse {
     
     public static void main(String[] args) {
-        QueueUsingArray queue = new QueueUsingArray();
+        QueueUsingArray queue = new QueueUsingArray(3);
+        
 
         for(int i = 1; i <= 5; i++){
             try {
