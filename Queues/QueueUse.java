6848cbadf4febf73917bc3e8c9787c7c1e46d6e0
sourabh <sourabhbarnwal2001@gmail.com>
2022-03-15 16:34:44 +0530

    Inbulit queue class using linked list

--- Queues/QueueUse.java
+++ Queues/QueueUse.java
@@ -4,19 +4,20 @@ public class QueueUse {
     
     public static void main(String[] args) {
         QueueUsingArray queue = new QueueUsingArray(3);
-        
+        QueueUsingLL<Integer> queue1 = new QueueUsingLL<>();
 
         for(int i = 1; i <= 5; i++){
-            try {
-                queue.enqueue(i);
-            } catch (QueueFullException e) {
-                
-            }
+            // try {
+            //     queue.enqueue(i);
+            // } catch (QueueFullException e) {
+
+            // }
+            queue1.enqueue(i);
         }
 
-        while(!queue.isEmpty()){
+        while(!queue1.isEmpty()){
             try {
-                System.out.println(queue.dequeue());
+                System.out.println(queue1.dequeue());
             } catch (QueueEmptyException e) {
                
                 e.printStackTrace();
