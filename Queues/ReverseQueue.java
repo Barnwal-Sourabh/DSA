c08b3f078d370b9aadc51101157d2c54d75b330e
sourabh <sourabhbarnwal2001@gmail.com>
2022-03-15 20:11:44 +0530

    Full queue reverse

---
+++ Queues/ReverseQueue.java
@@ -0,0 +1,58 @@
+package Queues;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ReverseQueue {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static Queue<Integer> takeInput() throws NumberFormatException, IOException {
+
+        int n = Integer.parseInt(br.readLine().trim());
+
+        String[] values = br.readLine().trim().split(" ");
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            queue.add(Integer.parseInt(values[i]));
+        }
+
+        return queue;
+
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+            Queue<Integer> queue = takeInput();
+            ReverseQueue.reverseQueue(queue);
+
+            while (!queue.isEmpty()) {
+                System.out.print(queue.poll() + " ");
+            }
+
+            System.out.println();
+
+            t -= 1;
+        }
+    }
+
+    public static void reverseQueue(Queue<Integer> input) {
+        if (input.size() <= 1) {
+            return;
+        }
+
+        int front = input.poll();
+        reverseQueue(input);
+        input.add(front);
+
+    }
+
+}
