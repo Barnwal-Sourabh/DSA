3e7dcd12846f94deb51521860b1b8fa92e475105
sourabh <sourabhbarnwal2001@gmail.com>
2022-03-15 16:36:52 +0530

    Check QueueUsingLL class

--- Queues/QueueUse.java
+++ Queues/QueueUse.java
@@ -3,7 +3,7 @@ package Queues;
 public class QueueUse {
     
     public static void main(String[] args) {
-        QueueUsingArray queue = new QueueUsingArray(3);
+        // QueueUsingArray queue = new QueueUsingArray(3);
         QueueUsingLL<Integer> queue1 = new QueueUsingLL<>();
 
         for(int i = 1; i <= 5; i++){
