cac1c1a4dec5adc7dba96c5dc418b8b1b94eb935
sourabh <sourabhbarnwal2001@gmail.com>
2022-03-19 10:32:23 +0530

    Tree use class

--- Tree/TreeUse.java
+++ Tree/TreeUse.java
@@ -1,19 +1,92 @@
 package Tree;
 
-public class TreeUse {
-    
-    public static void main(String[] args) {
-         
-        TreeNode<Integer> root = new TreeNode<Integer>(4);
-        TreeNode<Integer> node1 = new TreeNode<Integer>(2);
-        TreeNode<Integer> node2 = new TreeNode<Integer>(3);
-        TreeNode<Integer> node3 = new TreeNode<Integer>(5);
-        TreeNode<Integer> node4 = new TreeNode<Integer>(6);
 
-        root.children.add(node1);
-        root.children.add(node2);
-        root.children.add(node3);
-        node2.children.add(node4);
+
+import java.util.Scanner;
+
+import Queues.QueueEmptyException;
+import Queues.QueueUsingLL;
+
+
+
+public class TreeUse {
+
+    public static TreeNode<Integer> takeInput(Scanner s){
+        int n;
+       
+        System.out.println("Enter next node data");
+        n = s.nextInt();
+        TreeNode<Integer> root = new TreeNode<Integer>(n);
+        System.out.println("Enter number of children for " + n);
+        int chilCount = s.nextInt();
+
+        for(int i = 0; i<chilCount; i++){
+            TreeNode<Integer> child = takeInput(s);
+            root.children.add(child);
+        }
+        
+        return root;
+    }
+    public static void print(TreeNode<Integer> root){
+        String s = root.data + ":";
+        
+        for(int i = 0; i<root.children.size();i++){
+            s = s + root.children.get(i).data + ",";
+        }
+        System.out.println(s);
+        for(int i = 0; i<root.children.size(); i++){
+            print(root.children.get(i));
+        }
+
+
+    }
+
+    public static TreeNode<Integer> takeInputLevelwise(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter root data");
+        int rootData = s.nextInt();
+        QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+        TreeNode<Integer> root = new TreeNode<Integer>(rootData);
+        pendingNodes.enqueue(root);
+
+        while(!pendingNodes.isEmpty()){
+           try {
+            TreeNode<Integer> frontNode = pendingNodes.dequeue();
+            System.out.println("Enter the number of children of "+ frontNode.data);
+            int numchildren = s.nextInt();
+            for(int i = 0; i < numchildren; i++){
+                System.out.println("Enter " + (i+1) + "th child of "+frontNode.data);
+                int child = s.nextInt();
+                TreeNode<Integer> childNode = new TreeNode<Integer>(child);
+                frontNode.children.add(childNode);
+                pendingNodes.enqueue(childNode);
+            }
+
+        } catch (QueueEmptyException e) {
+            //Shoudn't come here
+        }
+
+        }
+
+        return root;
+        
+    }
+
+    public static void main(String[] args) {
+        // Scanner s = new Scanner(System.in);
+        // TreeNode<Integer> root = takeInput(s);
+        TreeNode<Integer> root = takeInputLevelwise();
+        print(root);
+        // TreeNode<Integer> root = new TreeNode<Integer>(4);
+        // TreeNode<Integer> node1 = new TreeNode<Integer>(2);
+        // TreeNode<Integer> node2 = new TreeNode<Integer>(3);
+        // TreeNode<Integer> node3 = new TreeNode<Integer>(5);
+        // TreeNode<Integer> node4 = new TreeNode<Integer>(6);
+
+        // root.children.add(node1);
+        // root.children.add(node2);
+        // root.children.add(node3);
+        // node2.children.add(node4);
         
     }
 }
