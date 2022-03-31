34cff148f878cabd10ff6a29f5cedf0efe3d538f
sourabh <sourabhbarnwal2001@gmail.com>
2022-03-31 20:10:24 +0530

    Print Tree

--- BinaryTree1/PrintTree.java
+++ BinaryTree1/PrintTree.java
@@ -2,95 +2,14 @@ package BinaryTree1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-class QueueEmptyException extends Exception {
-
-}
-
- class QueueUsingLL<T> {
-
-	class Node<T> {
-		T data;
-		Node<T> next;
-		Node(T data){
-			this.data = data;
-		}
-	}
-	
-	private Node<T> head;
-	private Node<T> tail;
-	private int size = 0;
-
-	public int size(){
-		return size;
-	}
-
-	public boolean isEmpty(){
-		if(size == 0){
-			return true;
-		}
-		return false;
-	}
-
-	public T front() throws QueueEmptyException{
-		if(size == 0){
-			QueueEmptyException e = new QueueEmptyException();
-			throw e;
-		}
-
-		return head.data;
-	}
-
-
-	public void enqueue(T element){
-		Node<T> newNode = new Node<T>(element);
-
-		if(head == null){
-			head = newNode;
-			tail = newNode;
-		}
-		else{
-			tail.next = newNode;
-			tail = newNode;
-		}
-
-		size++;
-	}
-
-	public T dequeue() throws QueueEmptyException{
-		if(head == null){
-			QueueEmptyException e = new QueueEmptyException();
-			throw e;
-		}
-		if(head == tail){
-			tail = null;
-		}
-		T temp = head.data;
-		head = head.next;
-		size--;
-		return temp;
-	}
-}
-
-
-class BinaryTreeNode<T> {
-	T data;
-	BinaryTreeNode<T> left;
-	BinaryTreeNode<T> right;
-
-	public BinaryTreeNode(T data) {
-		this.data = data;
-		this.left = null;
-		this.right = null;
-	}
-}
+import java.util.*;
 
 public class PrintTree {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
-		QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); 
+		Queue<BinaryTreeNode<Integer>>  pendingNodes = new LinkedList<BinaryTreeNode<Integer>>(); 
 		int start = 0;
 
 		String[] nodeDatas = br.readLine().trim().split(" ");
@@ -103,13 +22,13 @@ public class PrintTree {
 		start += 1;
 
 		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
-		pendingNodes.enqueue(root);
+		pendingNodes.add(root);
 
 		while(!pendingNodes.isEmpty()){
 			BinaryTreeNode<Integer> currentNode;
 			try {
-				currentNode = pendingNodes.dequeue();
-			} catch (QueueEmptyException e) {
+				currentNode = pendingNodes.poll();
+			} catch (Exception e) {
 				return null;
 			}
 
@@ -119,7 +38,7 @@ public class PrintTree {
 			if(leftChildData != -1){
 				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
 				currentNode.left = leftChild;
-				pendingNodes.enqueue(leftChild);
+				pendingNodes.add(leftChild);
 			}
 
 			int rightChildData = Integer.parseInt(nodeDatas[start]);
@@ -128,7 +47,7 @@ public class PrintTree {
 			if(rightChildData != -1){
 				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
 				currentNode.right = rightChild;
-				pendingNodes.enqueue(rightChild);
+				pendingNodes.add(rightChild);
 			}
 		}
 
@@ -144,14 +63,14 @@ public class PrintTree {
 		if(root == null){
         	return;
         }
-        QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); 
-        pendingNodes.enqueue(root);
+        Queue<BinaryTreeNode<Integer>>  pendingNodes = new LinkedList<BinaryTreeNode<Integer>>(); 
+        pendingNodes.add(root);
         
         while(!pendingNodes.isEmpty()){
             BinaryTreeNode<Integer> front;
 			try {
-				front = pendingNodes.dequeue();
-			} catch (QueueEmptyException e) {
+				front = pendingNodes.poll();
+			} catch (Exception e) {
 				return;
 			}
             
@@ -163,7 +82,7 @@ public class PrintTree {
             }else{   
 				System.out.print(front.data + ":");
                 if(front.left != null){
-        			pendingNodes.enqueue(front.left);
+        			pendingNodes.add(front.left);
 					System.out.print("L:" + front.left.data + ",");            
 					
                 }else {
@@ -171,7 +90,7 @@ public class PrintTree {
                 }
                 
                 if(front.right != null){
-            		pendingNodes.enqueue(front.right);
+            		pendingNodes.add(front.right);
                     System.out.print("R:" + front.right.data); 
                    
         		}else {
