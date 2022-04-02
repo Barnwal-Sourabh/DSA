4baed28c7c2724009d0dda1d149458bbfd0d62c3
sourabh <sourabhbarnwal2001@gmail.com>
2022-04-02 17:03:39 +0530

    MirrorBinaryTree

---
+++ BinaryTree1/MirrorBinaryTree.java
@@ -0,0 +1,108 @@
+package BinaryTree1;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class MirrorBinaryTree {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+		Queue<BinaryTreeNode<Integer>>  pendingNodes = new LinkedList<BinaryTreeNode<Integer>>(); 
+		int start = 0;
+
+		String[] nodeDatas = br.readLine().trim().split(" ");
+
+		if (nodeDatas.length == 1) {
+			return null;
+		}
+
+		int rootData = Integer.parseInt(nodeDatas[start]);
+		start += 1;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.add(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.poll();
+			} catch (Exception e) {
+				return null;
+			}
+
+			int leftChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.add(leftChild);
+			}
+
+			int rightChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.add(rightChild);
+			}
+		}
+
+		return root;
+	}
+
+
+	private static void printLevelWise(BinaryTreeNode<Integer> root){
+		Queue<BinaryTreeNode<Integer>>  primary = new LinkedList<>();
+		Queue<BinaryTreeNode<Integer>>  secondary = new LinkedList<>();
+
+		primary.add(root);
+
+		while(!primary.isEmpty()){
+			BinaryTreeNode<Integer> current=null;
+			try {
+				current = primary.poll();
+			} catch (Exception e) {
+				System.out.println("Not possible");
+			}
+			System.out.print(current.data + " ");
+			if(current.left != null){
+				secondary.add(current.left);
+			}
+			if(current.right != null){
+				secondary.add(current.right);
+			}
+			if(primary.isEmpty()){
+		        Queue<BinaryTreeNode<Integer>>  temp = secondary;
+				secondary = primary;
+				primary = temp;
+				System.out.println();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		
+		MirrorBinaryTree.mirrorBinaryTree(root);
+		printLevelWise(root);
+		
+	}
+
+    public static void mirrorBinaryTree(BinaryTreeNode<Integer> root){
+			if(root == null){
+				return;
+			}
+			
+			BinaryTreeNode<Integer> temp = root.left;
+			root.left = root.right;
+			root.right = temp;
+			
+			mirrorBinaryTree(root.left);
+			mirrorBinaryTree(root.right);
+		
+	}
+}
