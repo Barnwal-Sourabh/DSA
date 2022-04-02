4ba3ef56c5c9de8682253fa6d58d78016e174a50
sourabh <sourabhbarnwal2001@gmail.com>
2022-04-02 13:20:15 +0530

    FindNode

---
+++ BinaryTree1/FindNode.java
@@ -0,0 +1,83 @@
+package BinaryTree1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*; 
+
+public class FindNode {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+		Queue<BinaryTreeNode<Integer>>  pendingNodes = new LinkedList<BinaryTreeNode<Integer>>(); 
+		int start = 0;
+
+		String[] nodeDatas = br.readLine().trim().split(" ");
+
+		if (nodeDatas.length == 1) {
+			return null;
+		}
+
+		int rootData = Integer.parseInt(nodeDatas[start]);
+		start += 1;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.add(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.poll();
+			} catch (Exception e) {
+				return null;
+			}
+
+			int leftChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.add(leftChild);
+			}
+
+			int rightChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.add(rightChild);
+			}
+		}
+
+		return root;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		int x = Integer.parseInt(br.readLine().trim());
+
+		boolean ans = FindNode.isNodePresent(root, x);
+		
+		System.out.println(ans);
+	}
+
+    static boolean ans = false;
+    public static boolean isNodePresent(BinaryTreeNode<Integer> root, int x) {
+	    if(root == null){
+            return false;
+        }
+        
+        if(root.data == x){
+            ans = true;
+        }
+        
+        isNodePresent(root.left, x);
+       isNodePresent(root.right, x);
+            
+        return ans;
+        
+	}
+}
