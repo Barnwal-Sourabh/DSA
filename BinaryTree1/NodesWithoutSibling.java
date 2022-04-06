8c75e1c710af0d5e2fe6c5c00e45dee3908da808
sourabh <sourabhbarnwal2001@gmail.com>
2022-04-06 23:38:04 +0530

    Nodes without sibling

---
+++ BinaryTree1/NodesWithoutSibling.java
@@ -0,0 +1,84 @@
+package BinaryTree1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class NodesWithoutSibling {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+		Queue<BinaryTreeNode<Integer>>  pendingNodes = new LinkedList<BinaryTreeNode<Integer>>(); 
+		int start = 0;
+
+		String[] nodeDatas = br.readLine().trim().split(" ");
+
+		if (nodeDatas.length == 1) {
+			return null;
+		}
+
+		int rootData = Integer.parseInt(nodeDatas[start]);
+		start += 1;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.add(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.poll();
+			} catch (Exception e) {
+				return null;
+			}
+
+			int leftChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.add(leftChild);
+			}
+
+			int rightChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.add(rightChild);
+			}
+		}
+
+		return root;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+
+		NodesWithoutSibling.printNodesWithoutSibling(root);
+		
+	}
+
+	public static void printNodesWithoutSibling(BinaryTreeNode<Integer> root){
+		if(root == null){
+			return;
+		}
+		if(root.left == null && root.right == null){
+			return;
+		}
+
+		if(root.left == null){
+			System.out.println(root.right.data + " ");
+			printNodesWithoutSibling(root.right);
+		}else if(root.right == null){
+			System.out.println(root.left.data +" ");
+			printNodesWithoutSibling(root.left);
+		}else {
+			printNodesWithoutSibling(root.right);
+			printNodesWithoutSibling(root.left);
+		}
+	}
+}
