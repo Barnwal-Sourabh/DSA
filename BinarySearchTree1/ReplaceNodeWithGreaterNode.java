6aa10f1559bf9edd3418490c5e909891d8bc25f0
sourabh <sourabhbarnwal2001@gmail.com>
2022-05-19 01:06:11 +0530

    Replace node with greater node

---
+++ BinarySearchTree1/ReplaceNodeWithGreaterNode.java
@@ -0,0 +1,98 @@
+package BinarySearchTree1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class ReplaceNodeWithGreaterNode {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	public static BinaryTreeNode<Integer> takeInput() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int rootData = Integer.parseInt(st.nextToken());
+		if (rootData == -1) {
+			return null;
+		}
+		Queue<BinaryTreeNode<Integer>> pendingNodes = new LinkedList<BinaryTreeNode<Integer>>();
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.add(root);
+
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.poll();
+			} catch (Exception e) {
+				return null;
+			}
+			int leftChildData = Integer.parseInt(st.nextToken());
+			if (leftChildData != -1) {
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.add(leftChild);
+			}
+			int rightChildData = Integer.parseInt(st.nextToken());
+			if (rightChildData != -1) {
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.add(rightChild);
+			}
+		}
+		return root;
+	}
+
+	public static void printLevelWiseAtDiffLevel(BinaryTreeNode<Integer> root) {
+		if (root == null)
+			return;
+		Queue<BinaryTreeNode<Integer>> q = new LinkedList<BinaryTreeNode<Integer>>();
+		q.add(root);
+		q.add(null);
+		while (!q.isEmpty()) {
+			BinaryTreeNode<Integer> first = q.peek();
+			q.poll();
+			if (first == null) {
+				if (q.isEmpty()) {
+					break;
+				}
+				System.out.println();
+				q.add(null);
+				continue;
+			}
+			System.out.print(first.data + " ");
+			if (first.left != null) {
+				q.add(first.left);
+			}
+			if (first.right != null) {
+				q.add(first.right);
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		ReplaceNodeWithGreaterNode.replaceWithLargerNodesSum(root);
+		printLevelWiseAtDiffLevel(root);
+	}
+    public static void replaceWithLargerNodesSum(BinaryTreeNode<Integer> root) {
+		helperReplaceWithLargerNodesSum(root, 0);
+
+	}
+    public static int helperReplaceWithLargerNodesSum(BinaryTreeNode<Integer> root, int sum){
+        if(root == null){
+            return 0;
+        }
+        
+        int rightsum = helperReplaceWithLargerNodesSum(root.right, sum);
+        int rootdata = root.data;
+        root.data = rightsum + root.data + sum;
+        int leftsum = helperReplaceWithLargerNodesSum(root.left, root.data);
+        
+        return rightsum + rootdata + leftsum;
+        
+        
+    }
+
+}
