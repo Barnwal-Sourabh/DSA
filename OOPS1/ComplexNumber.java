47bdf61da06f8531ddeb1ff447c82d3d439005c5
sourabh <sourabhbarnwal2001@gmail.com>
2022-06-25 19:25:50 +0530

    ComplexNumber

--- OOPS1/ComplexNumber.java
+++ OOPS1/ComplexNumber.java
@@ -1,50 +1,44 @@
 package OOPS1;
 
-import java.util.Scanner;
-
 public class ComplexNumber {
+    private int real;
+    private int imaginary;
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-
-		int real1 = s.nextInt();
-		int imaginary1 = s.nextInt();
-
-		int real2 = s.nextInt();
-		int imaginary2 = s.nextInt();
-
-		ComplexNumber c1 = new ComplexNumber(real1, imaginary1);
-		ComplexNumber c2 = new ComplexNumber(real2, imaginary2);
-
-		int choice = s.nextInt();
-
-        
-        s.close();
-		if(choice == 1) {
-            // Add
-			c1.plus(c2);
-			c1.print();
-		}
-		else if(choice == 2) {
-			// Multiply
-			c1.multiply(c2);
-			c1.print();
-		}
-		else {
-			return;
-		}
-	}
-    int real;
-     int imaginary;
-    
-	public ComplexNumber(int real, int imaginary ){
+    public ComplexNumber(int real, int imaginary){
         this.real = real;
         this.imaginary = imaginary;
     }
-    public void plus(ComplexNumber c1){
-        this.real = this.real + c1.real;
-        this.imaginary = this.imaginary + c1.imaginary;
+
+    public void setImaginary(int imaginary){
+        this.imaginary = imaginary;
     }
+    public void setReal(int real){
+        this.real = real;
+    }
+
+    public int getImaginary(){
+        return imaginary;
+    }
+    
+    public int getReal(){
+        return real;
+    }
+
+    public void print(){
+        if(imaginary > 1){
+            System.out.println(real +" + i"+imaginary);
+        }else if(imaginary == 1) {
+            System.out.println(real +" + i");
+        }else {
+            System.out.println(real);
+        }
+    }
+
+    public void add(ComplexNumber c2){
+        this.real = this.real + c2.real;
+        this.imaginary = this.imaginary + c2.imaginary;
+    }
+
     public void multiply(ComplexNumber c1){
         int i,r;
         i = (this.real * c1.imaginary) + (this.imaginary * c1.real);
@@ -52,8 +46,8 @@ public class ComplexNumber {
         this.real = r;
         this.imaginary = i;
     }
-	public void print(){
-        System.out.println(real+" + i" + imaginary);
-    }
-}
 
+    
+
+   
+}
