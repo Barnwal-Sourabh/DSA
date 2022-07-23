e2aa6d8da0c2105c2a5cac83748b48c901067f22
sourabh <sourabhbarnwal2001@gmail.com>
2022-07-23 23:20:51 +0530

    all commond same

--- HashMap/MapNode.java
+++ HashMap/MapNode.java
@@ -3,11 +3,11 @@ package HashMap;
 public class MapNode<K, V> {
     K key;
     V value;
-    MapNode<K,V> next;
+    MapNode<K, V> next;
 
-    public MapNode(K key, V value){
+    public MapNode(K key, V value) {
         this.key = key;
         this.value = value;
     }
-    
+
 }
