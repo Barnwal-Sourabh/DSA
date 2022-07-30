3ff9816540c5e86949e8f296453645708599afdb
sourabh <sourabhbarnwal2001@gmail.com>
2022-07-31 01:19:41 +0530

    add remove method

--- TriesAndHuffman/Trie.java
+++ TriesAndHuffman/Trie.java
@@ -4,11 +4,13 @@ class TrieNode {
     char data;
     boolean isTerminating;
     TrieNode children[];
+    int childCount;
 
     TrieNode(char data) {
         this.data = data;
         isTerminating = false;
         children = new TrieNode[26];
+        childCount = 0;
     }
 
 }
@@ -32,6 +34,7 @@ public class Trie {
         if (child == null) {
             child = new TrieNode(word.charAt(0));
             root.children[childIndex] = child;
+            root.childCount++;
         }
 
         add(child, word.substring(1));
@@ -55,6 +58,32 @@ public class Trie {
 
     public boolean search(String word){
         return search(root, word);
+    }
+
+    
+    public void remove(String word){
+        remove(root,word);
+    }
+
+    private void remove(TrieNode root, String word){
+        if(word.length() == 0){
+            root.isTerminating = false;
+            return;
+        }
+
+        int childIndex = word.charAt(0) - 'a';
+        TrieNode child = root.children[childIndex];
+        if(child == null){
+            return;
+        }
+        remove(child, word.substring(1));
+
+        if(!child.isTerminating && child.childCount == 0){
+            root.children[childIndex] = null;
+            child = null;
+            root.childCount--;
+        }
+
     }
 
 }
