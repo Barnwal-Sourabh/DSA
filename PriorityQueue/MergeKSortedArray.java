e7fb7292c343026cb60d6329156381b2253effd7
sourabh <sourabhbarnwal2001@gmail.com>
2022-07-31 01:04:44 +0530

    MergeKSortedArray

--- PriorityQueue/MergeKSortedArray.java
+++ PriorityQueue/MergeKSortedArray.java
@@ -3,8 +3,8 @@ package PriorityQueue;
 import java.io.*;
 import java.util.*;
 
-class PairComparator implements Comparator<Pair> {
-    public int compare(Pair p1, Pair p2) {
+class PairsComparator implements Comparator<Pairs> {
+    public int compare(Pairs p1, Pairs p2) {
         if(p1.value <= p2.value){
             return 1;
         }else{
@@ -12,10 +12,10 @@ class PairComparator implements Comparator<Pair> {
         }
     }
 }
-class Pair {
+class Pairs {
     int value;
     int arrayNumber;
-    Pair(int v, int n) {
+    Pairs(int v, int n) {
         value = v;
         arrayNumber = n;
     }
@@ -73,25 +73,25 @@ public class MergeKSortedArray {
         if (input.size()==0){
             return null;
         }
-        PairComparator compare = new PairComparator();
-        PriorityQueue<Pair> maxHeap = new PriorityQueue<Pair>(input.size(),
+        PairsComparator compare = new PairsComparator();
+        PriorityQueue<Pairs> maxHeap = new PriorityQueue<Pairs>(input.size(),
         compare);
         for(int i = 0; i < input.size(); i++) {
             int index = input.get(i).size() - 1;
-            Pair current = new Pair(input.get(i).get(index), i);
+            Pairs current = new Pairs(input.get(i).get(index), i);
             maxHeap.add(current);
             input.get(i).remove(index);
         }
         ArrayList<Integer> output = new ArrayList<Integer>();
         while(!maxHeap.isEmpty()) {
-            Pair max = maxHeap.remove();
+            Pairs max = maxHeap.remove();
             output.add(max.value);
             int i = max.arrayNumber;
             if(input.get(i).size() > 0) {
                 int index = input.get(i).size() - 1;
                 int value = input.get(i).get(index);
                 input.get(i).remove(index);
-                Pair current = new Pair(value, i);
+                Pairs current = new Pairs(value, i);
                 maxHeap.add(current);
             }
         }
