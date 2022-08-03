95f19e0f9f7c12fc85c8a96c4882ba126e82f57f
sourabh <sourabhbarnwal2001@gmail.com>
2022-08-03 14:43:01 +0530

    Min steps two one

--- DynamicProgramming/MinStepsTwoOne.java
+++ DynamicProgramming/MinStepsTwoOne.java
@@ -10,6 +10,8 @@ public class MinStepsTwoOne {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         int n = Integer.parseInt(br.readLine().trim());
+        System.out.println(MinStepsTwoOne.countM(n));
+        System.out.println(MinStepsTwoOne.countStepsDP(n));
         System.out.println(MinStepsTwoOne.countMinStepsToOne(n));
     }
 
@@ -30,6 +32,59 @@ public class MinStepsTwoOne {
             divideByThree = countMinStepsToOne(n / 2);
         }
         return 1 + Math.min(subtractOne, Math.min(divideByThree, divideByTwo));
+    }
+
+    //Memoization technique
+    public static int countM(int n) {
+        int storage[] = new int[n + 1];
+        return countM(n, storage);
+    }
+
+    private static int countM(int n, int[] storage) {
+        if (n == 1) {
+            storage[n] = 0;
+            return storage[n];
+        }
+
+        if (storage[n] != 0) {
+            return storage[n];
+        }
+
+        int opt1 = countM(n - 1, storage);
+        int minStep = opt1;
+        if (n % 3 == 0) {
+            int opt2 = countM(n / 3, storage);
+            minStep = Math.min(opt2, minStep);
+        }
+        if (n % 2 == 0) {
+            int opt3 = countM(n / 2, storage);
+            minStep = Math.min(opt3, minStep);
+        }
+
+        storage[n] = 1 + minStep;
+        return storage[n];
+    }
+
+    //DP technique
+    public static int countStepsDP(int n){
+        int storage[] = new int[n+1];
+        storage[1] = 0;
+
+        for(int i =2; i<=n; i++){
+            int max = storage[i-1];
+            if(i % 3 == 0){
+                if(max > storage[i/3]){
+                    max = storage[i/3];
+                }
+            }
+            if(i % 2 == 0){
+                if(max > storage[i/2]){
+                    max = storage[i/2];
+                }
+            }
+            storage[i] = 1 + max;
+        }
+        return storage[n];
     }
 
 }
