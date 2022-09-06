17821b61e897643c65328abe32988038852a7b1c
sourabh <sourabhbarnwal2001@gmail.com>
2022-09-06 20:54:36 +0530

    Check bst

--- BinarySearchTree1/CheckBST3.java
+++ BinarySearchTree1/CheckBST3.java
@@ -2,7 +2,9 @@ package BinarySearchTree1;
 
 
 public class CheckBST3 {
-    
+    public static boolean isBST3(BinaryTreeNode<Integer> root){
+        return isBST3(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
     public static boolean isBST3(BinaryTreeNode<Integer> root, int min, int max){
         if(root == null){
             return false;
